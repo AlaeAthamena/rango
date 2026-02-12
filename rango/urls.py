4d2d2b1e10065592a10240c07ce93c892aa39f4e
AlaeAthamena <alaeathamena@icloud.com>
2026-02-12 10:50:31 +0000

    Set up data-driven homepage and category pages
    Connected Rango models, views, and templates to serve dynamic content, updated the index view to show the top 5 categories, created a show_category view and template to display pages for each category, added slugs to Category with admin auto-population, and populated the database using populate_rango.py for testing and demonstration.

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
-app_name = 'rango'
+from rango import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
