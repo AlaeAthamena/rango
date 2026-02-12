d9cc831e66771d29827eb20a237c448fece0ffaf
AlaeAthamena <alaeathamena@icloud.com>
2026-02-12 10:58:29 +0000

    Add most liked categories and most viewed pages to homepage
    Updated the index view and template to display the top five most liked categories and the top five most viewed pages. Added <h2> headers for clarity and ensured both sections render as clickable links, with a fallback message when no data is present.

--- rango/views.py
+++ rango/views.py
@@ -17,9 +17,16 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
+
+
+    page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {
         'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-        'categories': category_list
+        'categories': category_list,
+        'pages': page_list,
     }
+
     return render(request, 'rango/index.html', context=context_dict)
