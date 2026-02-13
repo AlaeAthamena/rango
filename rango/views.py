dae3c03e0a545fda0a2917fc10ca2cecbbeeecb4
AlaeAthamena <alaeathamena@icloud.com>
2026-02-13 01:44:44 +0000

    Implemented cookies and session-based visit tracking
    Added cookie and session functionality to track user visits in the Rango app by implementing a visitor_cookie_handler() helper function and integrating it into the index() view. This allows the application to store and update visit-related data (visits and last_visit) using Django sessions, enabling persistent tracking of returning users across requests.

--- rango/views.py
+++ rango/views.py
@@ -9,9 +9,44 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(
+        request,
+        'last_visit',
+        str(datetime.now())
+    )
+
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7],
+        '%Y-%m-%d %H:%M:%S'
+    )
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 def about(request):
-    return render(request, 'rango/about.html')
+    visitor_cookie_handler(request)
+    context_dict = {'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context=context_dict)
+def contact(request):
+    return render(request, 'rango/contact.html')
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -26,19 +61,19 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
-
     category_list = Category.objects.order_by('-likes')[:5]
-
-
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-        'categories': category_list,
-        'pages': page_list,
-    }
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
     return render(request, 'rango/index.html', context=context_dict)
+
 @login_required
 def add_category(request):
     form = CategoryForm()
